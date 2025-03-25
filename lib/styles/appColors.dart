323546610d062a0f22371082352d247ed30fa2dc
Matheus Lula <114251220+MatheusConaga@users.noreply.github.com>
2025-03-25 20:07:14 -0300

    teste do html

--- lib/styles/appColors.dart
+++ lib/styles/appColors.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color darkblue = Color(0xFF080322);
   static const Color blue = Color(0xFF0138FF);
+  static const Color bgblue = Color(0xFF002FFF);
   static const Color white = Color(0xFFFFFFFF);
+
 }
